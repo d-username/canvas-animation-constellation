1920c53dfac3fabed49918a2d00a0a199f4d3aef
David <czdavid93@gmail.com>
2022-07-20 22:53:29 +0200

    particles are moving freely, and bouncing back from walls

--- script.js
+++ script.js
@@ -21,12 +21,17 @@ class Particle {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.size = 3;
+    this.size = 5;
     this.density = Math.random() * 5 + 1;
+    this.directionAngle = Math.random() * 360;
+    this.speed = 1 + Math.random() * 1;
+    this.vector = {
+      x: Math.cos(this.directionAngle) * this.speed,
+      y: Math.sin(this.directionAngle) * this.speed,
+    };
   }
 
   draw() {
-    // context.scale(1, 1);
     context.fillStyle = 'rgba(38, 198, 218, 1)';
     context.beginPath();
     context.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
@@ -35,38 +40,24 @@ class Particle {
     context.stroke();
   }
 
-  // DOCUMENTATION I USED:
-  // https://www.w3schools.com/tags/canvas_arc.asp
+  update() {
+    this.checkBorder();
+    this.x += this.vector.x;
+    this.y += this.vector.y;
+  }
 
-  //   update() {
-  //     let dx = mouse.x - this.x;
-  //     let dy = mouse.y - this.y;
-  //     let distance = Math.sqrt(dx * dx + dy * dy);
-  //     let forceDirectionX = dx / distance;
-  //     let forceDirectionY = dy / distance;
-  //     let maxDistance = mouse.radius;
-  //     let force = (maxDistance - distance) / maxDistance;
-  //     let directionX = forceDirectionX * force * this.density;
-  //     let directionY = forceDirectionY * force * this.density;
-
-  //     if (distance < mouse.radius) {
-  //       this.x -= directionX;
-  //       this.y -= directionY;
-  //     } else if (
-  //       distance >= mouse.radius &&
-  //       (this.x !== this.baseX || this.y !== this.baseY)
-  //     ) {
-  //       this.size = 3;
-  //       let dx = this.x - this.baseX;
-  //       this.x -= dx / 25;
-  //       let dy = this.y - this.baseY;
-  //       this.y -= dy / 25;
-  //     }
-  //   }
+  checkBorder() {
+    if (this.x >= canvas.width || this.x <= 0) {
+      this.vector.x *= -1;
+    }
+    if (this.y >= canvas.height || this.y <= 0) {
+      this.vector.y *= -1;
+    }
+  }
 }
 
 function createParticles() {
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 50; i++) {
     let x = Math.random() * canvas.width;
     let y = Math.random() * canvas.height;
     particleArray.push(new Particle(x, y));
@@ -76,14 +67,12 @@ function createParticles() {
 createParticles();
 
 function renderParticles() {
-  console.log('ciao');
-  //   context.clearRect(0, 0, canvas.width, canvas.height);
+  context.clearRect(0, 0, canvas.width, canvas.height);
   for (let i = 0; i < particleArray.length; i++) {
     particleArray[i].draw();
-    // particleArray[i].update();
+    particleArray[i].update();
   }
-  //   connect();
-  //   requestAnimationFrame(renderParticles);
+  //requestAnimationFrame(renderParticles);
 }
 
 renderParticles();
